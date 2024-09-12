26c388dae9a6091fda43ea94a2d269ec4c6f1cb2
Chais666 <roman27102006@mail.ru>
2024-09-12 10:23:54 +0300

    laboratory work

---
+++ circles/circles.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <locale.h> 
+
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    double x1, y1, r1, x2, y2, r2, distance, pow1, pow2;
+
+    printf("Введите координаты x,y и радиус первой окружности r: ");
+    scanf_s("%lf %lf %lf", &x1, &y1, &r1);
+
+    printf("Введите координаты x,y и радиус второй окружности r: ");
+    scanf_s("%lf %lf %lf", &x2, &y2, &r2);
+
+    pow1 = pow((x1 - x2), 2.0);
+    pow2 = pow((y1 - y2), 2.0);
+
+    distance = sqrt(pow1 + pow2);
+    
+    if (distance < r1 + r2) {
+        printf("Окружности пересекаются");
+    }
+    
+    if (distance == r1 + r2) {
+        printf("Окружности касаются");
+    }
+    
+    if (distance > r1 + r2) {
+        printf("Окружности не пересекаются");
+    }
+    
+}
---
+++ hello world/hello world.cpp
@@ -0,0 +1,6 @@
+﻿#include <stdio.h>
+#include <math.h>
+void main()
+{
+	printf("Hello World!");
+}
